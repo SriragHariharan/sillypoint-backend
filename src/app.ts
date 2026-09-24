7bd3ac5b3698b082e85020e6e77f09004f7216a6
Srirag Hariharan <sriraghariharan108@gmail.com>
2026-09-24 10:55:07 +0530

    Add Drizzle ORM Postgres connection

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,7 @@
+import "dotenv/config"
 import express, { type Request, type Response } from "express"
+import { sql } from "drizzle-orm"
+import { db } from "./db/postgres.js"
 
 const app = express()
 
@@ -6,4 +9,14 @@ app.get("/test", (_req: Request, res: Response) => {
     res.json({message: "Hello world, sillypoint"})
 })
 
-app.listen(3000, () => console.log("App running on port: http://localhost:3000/test"))
+async function start() {
+    await db.execute(sql`select 1`)
+    console.log("Connected to database")
+
+    app.listen(3000, () => console.log("App running on port: http://localhost:3000/test"))
+}
+
+start().catch((err) => {
+    console.error("Failed to start server:", err)
+    process.exit(1)
+})
